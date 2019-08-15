7f97c83966e8c206cb943bbd7de0b6efba037b91
Dushyant Joshi <dushyant.joshi@people-print.com>
2019-08-15 20:02:25 +0530

    dev: final submission with readme file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CountriesTableSeeder::class);
+        $this->call(ContactAddressSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
